5cc0d3399e7de475b406a9b1ab044c5e1b3681eb
byunghoonpark <byunghoonpark1005@gmail.com>
2019-03-22 16:11:58 +0900

    src update

--- src/darknet/ttsitt.py
+++ src/darknet/ttsitt.py
@@ -1,7 +1,7 @@
 from google.cloud import texttospeech
 from google.cloud import vision
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="BeEyes-8a3077c1a3bc.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="google-api key"
 import io
 import pygame
 
